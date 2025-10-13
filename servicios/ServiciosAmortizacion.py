59a32d00392b91ad6f446052728e72b130044106
jajastur <fabia9000bc@gmail.com>
2025-10-13 14:28:00 -0500

    arreglo de los servicios

--- servicios/ServiciosAmortizacion.py
+++ servicios/ServiciosAmortizacion.py
@@ -4,20 +4,23 @@ from app.models.models import Amortizacion
 router=APIRouter()
 
 @router.post("/calcular_Amortizacion")
-def calcular_Amortizacion(data:Amortizacion):
-    if data.A is None:
+def calcular_Amortizacion_sistema_frances(data:Amortizacion):
         try:
             A=data.Amortizacion_Sistema_Frances()
             return {"Cuota de Amortizacion":A}
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
-    elif data.n is None:
+
+@router.post("/calcular_Amortizacion_Sistema_Americano")
+def calcular_Amortizacion_sistema_aleman(data:Amortizacion):
         try:
             i=data.Amortizacion_Sistema_Americano()
             return {"interes":i}
         except Exception as e:
             raise HTTPException(status_code=500, detail=str(e))
-    else:
+@router.post("/calcular_Amortizacion_Sistema_Aleman")
+def calcular_Amortizacion_sistema_aleman(data:Amortizacion):
+        A=[0,0,0]
         try:
             A[0,0,0,]=data.Amortizacion_Sistema_Aleman()
             return {"Cuota":A[0],"Amortizacion":A[1],"Interes":A[2]}
--- servicios/ServiciosGradiente.py
+++ servicios/ServiciosGradiente.py
@@ -2,18 +2,22 @@ from fastapi import FastAPI,APIRouter,HTTPException
 from app.models.models import Gradiente
 
 router=APIRouter()
-@router.post("/calcular_Gradiente")
+
+@router.post("/calcular_Gradiente_aritmetico")
 def calcular_Gradiente_aritmetico(data:Gradiente):
     try:
-        if data.g != 0 and data.G == 0:
-            vf=data.Valor_futuro_gradiente_aritmetico()
-            va=data.Valor_actual_gradiente_aritmetico()
-            return {"Valor Futuro Gradiente Aritmetico":vf,"Valor Actual Gradiente Aritmetico":va}
-        elif data.G != 0 and data.g == 0:
-            vf=data.Valor_futuro_gradiente_geometrico()
-            va=data.Valor_presente_gradiente_geometrico()
-            return {"Valor Futuro Gradiente Geometrico":vf,"Valor Actual Gradiente Geometrico":va}
-        else:
-            raise HTTPException(status_code=400, detail="Por favor ingrese solo uno de los dos tipos de gradiente (g o G)")
+        vf=data.Valor_futuro_gradiente_aritmetico()
+        va=data.Valor_actual_gradiente_aritmetico()
+        return {"Valor Futuro Gradiente Aritmetico":vf,"Valor Actual Gradiente Aritmetico":va}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@router.post("/calcular_Gradiente_geometrico")
+def calcular_Gradiente_geometrico(data:Gradiente):
+    try:    
+        vf=data.Valor_futuro_gradiente_geometrico()
+        va=data.Valor_presente_gradiente_geometrico()
+        return {"Valor Futuro Gradiente Geometrico":vf,"Valor Actual Gradiente Geometrico":va}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
